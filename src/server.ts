4cbccc68b704c285ff75cbc410da8d0908f5168f
CarlosWinicius <carloswinicius055@gmail.com>
2025-10-29 00:49:22 -0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // src/server.ts
 
 import {Elysia} from 'elysia';
+import { swagger } from '@elysiajs/swagger';
 
 // 1. Importe suas rotas
 import {urlRoutes} from './routes/UrlRoutes';
@@ -9,6 +10,7 @@ import {urlRoutes} from './routes/UrlRoutes';
 import urlService from './services/UrlService';
 
 new Elysia()
+    .use(swagger())
     // 3. Injete a INSTÂNCIA com um nome minúsculo
     .decorate('urlService', urlService)
 
